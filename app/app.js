658aa4d21887e2cd8fc4637d16302701dfd3c560
Dommmel <dommmel@gmail.com>
2014-08-01 11:52:53 +0200

    init

---
+++ app/app.js
@@ -0,0 +1,74 @@
+var express           = require('express')
+  , helmet            = require('helmet')
+  , csrf              = require('csurf')
+  , cookieParser      = require('cookie-parser')
+  , bodyParser        = require('body-parser')
+  , cookieSession     = require('cookie-session')
+  , methodOverride    = require('method-override')
+  , compress          = require('compression')
+  , swig              = require('swig')
+  , path              = require('path')
+  , fs                = require('fs')
+  , logger            = require("morgan")
+  , debug             = require('debug')('app');
+
+var app = express();
+var env = process.env.NODE_ENV || 'development';
+
+app.set('port', (process.env.PORT || 4000));
+app.use(compress());
+app.use(bodyParser());
+app.use(helmet.defaults())
+app.use(methodOverride());
+
+// Configure templating
+if (process.env.NODE_ENV !== 'production') {
+  swig.setDefaults({ cache: false });
+}
+app.engine('html.swig', swig.renderFile)
+app.set('view engine', 'html.swig');
+app.set('views', __dirname + '/views');
+
+// Enable cookie based sessions
+app.use(cookieParser("another secret secret"));  
+app.use(cookieSession({
+  secret: "super secret",
+}))
+
+// Logging
+app.use(logger("dev"));
+
+// Configure micer as asset pipeline
+require('./config/mincer')(app);
+
+// GET ALL THE ROUTES
+var routesPath = path.join(__dirname, 'routes');
+fs.readdirSync(routesPath).forEach(function(file) {
+  require(routesPath + '/' + file)(app);
+});
+
+// app.enable('trust proxy');
+
+/// Catch 404 and forward to error handler
+app.use(function(req, res, next) {
+  var err = new Error('Not Found');
+  err.status = 404;
+  next(err);
+});
+
+/// Error handlers
+app.use(function(err, req, res, next) {
+  console.error(err.stack);
+  var status = err.status || 500
+  res.status(status);
+  res.render('error', {
+    message: err.message,
+    error: ('development' == env) ? err : {}, // don't show stack trace in production
+    status: status
+  });
+});
+
+// START SERVER
+app.listen(app.get('port'), function() {
+  debug("Node app is running at localhost:" + app.get('port'))
+})
---
+++ app/config/mincer.js
@@ -0,0 +1,90 @@
+module.exports = function(app) {
+  var mincer = require('mincer'),
+      path   = require('path'),
+      debug  = require('debug')('gruessli:mincer');
+
+  mincer.logger.use({
+    error: function(level, msg) {
+      console.error('ERROR:', msg);
+    },
+    log: function(level, msg) {
+      debug(level +": "+ msg);
+    }
+  });
+  
+  var environment = new mincer.Environment(__dirname);
+  environment.enable('source_maps');// Enable source maps support
+  //environment.sourceRoot = '/'; // use to cheat nesting level in dev tools
+
+  // Configure environment load paths (where to find assets)
+  environment.appendPath('../assets/javascripts');
+  environment.appendPath('../assets/stylesheets');
+  environment.appendPath('../assets/images');
+  environment.appendPath('../assets/fonts');
+
+  // Define environment essential *_path helper that will be available in the
+  // processed assets. See `assets/stylesheets/app.css.ejs` for example.
+  environment.ContextClass.defineAssetPath(function (pathname, options) {
+    var asset = this.environment.findAsset(pathname, options);
+    if (!asset) { 
+      throw new Error("File " + pathname + " not found");
+    }
+    return '/assets/' + asset.digestPath;
+  });
+
+  // Enable auto-Prefixer
+  environment.enable("autoprefixer");
+
+  // Prepare production-ready environment
+  if ('production' === process.env.NODE_ENV) {
+    // Cache compiled assets.
+    environment.cache = new mincer.FileStore(path.join(__dirname, 'cache'));
+    // Enable JS and CSS compression
+    environment.jsCompressor  = "uglify";
+    // (!) use csswring, because csso does not supports sourcemaps
+    environment.cssCompressor = "csswring";
+    // cache environment. 
+    environment = environment.index;
+  }
+  app.use('/assets/', mincer.createServer(environment));
+
+  /* 
+   * Asset View helpers
+   * Allows you to call javascript("filename.js") or stylesheet("filename.css")
+   * in your templates.
+   */
+  function rewrite_extension(source, ext) {
+    var source_ext = path.extname(source);
+    return (source_ext === ext) ? source : (source + ext);
+  }
+
+  function alertAssetNotFound(fileType, logicalPath){
+      return '<script type="application/javascript">alert("' + fileType + ' file ' +
+             JSON.stringify(logicalPath).replace(/"/g, '\\"') +
+             ' not found.")</script>';
+  }
+
+  app.locals.asset_path = function(logicalPath) {
+    var asset = environment.findAsset(logicalPath);
+    if (asset) {
+      return '/assets/' + asset.digestPath
+    }
+  }
+
+  app.locals.javascript = function javascript(logicalPath) {
+    var asset = environment.findAsset(logicalPath);
+    if (!asset) alertAssetNotFound("Javascript", logicalPath);
+    return '<script type="application/javascript" src="/assets/' +
+            rewrite_extension(asset.digestPath, '.js') +
+            '"></script>';
+  };
+
+  app.locals.stylesheet = function stylesheet(logicalPath) {
+    var asset = environment.findAsset(logicalPath);
+    if (!asset) alertAssetNotFound("Stylesheet", logicalPath);
+    return '<link rel="stylesheet" type="text/css" href="/assets/' +
+            rewrite_extension(asset.digestPath, '.css') +
+            '" />';
+  };
+}
+
